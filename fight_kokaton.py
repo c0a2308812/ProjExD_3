5c7eb5c985cdb60325e9cf61859907f1d245cf0c
setoyama <c0a2308812@edu.teu.ac.jp>
2024-06-25 14:41:56 +0900

    練習問題3

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -170,6 +170,7 @@ def main():
             if beam.rct.colliderect(bomb.rct):  # ビームと爆弾の衝突判定
                 beam = None
                 bomb = None
+                bird.change_img(6, screen)  # こうかとんが喜ぶエフェクト
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
